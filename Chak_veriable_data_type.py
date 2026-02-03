4a6df83fd7343cb4c1637b831e896b59e3de54d4
RudroMondal <rudromondal151@gmail.com>
2026-02-04 00:22:27 +0600

    5th day project

---
+++ Chak_veriable_data_type.py
@@ -0,0 +1,7 @@
+ver_ty_1=2
+ver_ty_2=4.3
+ver_ty_3="Hello"
+
+print(type(ver_ty_1))
+print(type(ver_ty_2))
+print(type(ver_ty_3))
---
+++ Number_switching_code.py
@@ -0,0 +1,27 @@
+A=input("Enter the value of A: ")
+B=input("Enter the value of B: ")
+
+switch = A
+A = B
+B = switch
+
+print("Now A="+A)
+print("And B="+B)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
